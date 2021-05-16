ec4ef2fa478d589463ff1eae721b25afdd776471
Oscar Pinedo <opinedo19@gmail.com>
2021-05-16 16:53:01 -0700

    removed debug print statements and added comments

--- ninja_gold/ninja_gold_app/views.py
+++ ninja_gold/ninja_gold_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render, HttpResponse
 import random
 from datetime import datetime
 
+# dictionary for our gold value ranges
 GOLD_RANGES = {
     "farm": (10,20),
     "cave": (5,10),
@@ -22,33 +23,28 @@ def process_gold(request):
         return redirect("/")
     # checks which box was selected
     selected_building = request.POST['building']
-    print("selected building:", selected_building)
     # assigns range depending on box selected
     gold_range_for_building = GOLD_RANGES[selected_building]
-    print('gold range: ', gold_range_for_building)
     # generates random number for that range
     gold_change = random.randint(gold_range_for_building[0], gold_range_for_building[1])
-    # print('gold received: ', gold_change)
-
 
     activity_msg = "Earned {} gold from the {}!".format(gold_change, selected_building)
     # if convert sign is positve then casino will lose money
     result = "win"
     if selected_building == "casino":     
         convert_sign = random.randint(0,1)
-        print("Sign generated through random ind in range 0 - 1: ", convert_sign)
         if convert_sign:
             gold_change = gold_change * -1
             activity_msg = "Entered a {} and lost {} gold... Ouch..".format(selected_building, gold_change)
             result = "lost"
-    print('gold received: ', gold_change)
+
     # append the date and time to our activity message
     activity_msg += " {}".format(datetime.now().strftime("%m/%d/%Y %I:%M%p"))
-    print(activity_msg)
 
+    # updates our session values
     request.session['gold_bal'] += gold_change
     request.session['activities'].append({"msg":activity_msg, "result":result})
-    print(request.session['activities'])
+
 
 
     
