264d37b204ee62b81f0ca429f01680e48e2229ad
Oscar Pinedo <opinedo19@gmail.com>
2021-05-16 04:46:29 -0700

    added our initial session variables and tested server after adding migrations

--- ninja_gold/ninja_gold_app/views.py
+++ ninja_gold/ninja_gold_app/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import redirect, render, HttpResponse
 
 # Create your views here.
 def index(request):
+    # sets our inital conditions for our mini game
+    if not "gold" in request.session or "activities" not in request.session:
+        request.session['gold'] = 0
+        request.session['activities'] = []
     return render(request, "index.html")
+
+def process_gold(request):
+    if request.method == 'GET':
+        return redirect("/")
